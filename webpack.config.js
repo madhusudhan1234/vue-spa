e2d7e41adcc6ae7018fd80f63d6c32ca3500aa6a
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2017-06-15 01:59:25 +0545

    First Commit - Basic WebPack Setting

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var path = require('path');
+const isProduction = (process.env.NODE_ENV === 'production');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: {
+		app: [
+			'./scripts/main.js',
+			'./scss/main.scss'
+		]
+    },
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].js'
+	},
+	module: {
+        rules: [
+        	{
+            	test: /\.s[ac]ss$/,
+            	use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+					fallback: 'style-loader'
+				})
+        	},
+            {
+            	test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+            }
+        ]
+	},
+	plugins: [
+        new ExtractTextPlugin("[name].css"),
+        new webpack.LoaderOptionsPlugin({
+            minimize: isProduction,
+        }),
+    ]
+}
+if(isProduction) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            sourcemap: true,
+            compress: {
+                warnings: false
+            }
+        })
+    );
+};
