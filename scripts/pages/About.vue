7c7c06e061beef26a5b3a1d1b322905234438c1d
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2017-07-03 20:39:57 +0545

    Some changes

--- scripts/pages/About.vue
+++ scripts/pages/About.vue
@@ -3,7 +3,7 @@
 		<div class="columns">
 		  <div class="column">
 		  	<h1 class="title">About Me</h1>
-		  	<p>
+		  	<p class="about-text">
 				Experienced Web Developer with a demonstrated history of working in the computer software industry. Skilled in Mysql, C++, PHP, Javascript, Google Analytics, and Paypal Integration. Working experiences with Javascript Frameworks like Angular js, Vue js, and PHP Framework Laravel. Strong engineering professional knowledge focused in Bachelor of Science Computer Science and Information Technology from Academia International College.
 			</p>
 		  </div>
